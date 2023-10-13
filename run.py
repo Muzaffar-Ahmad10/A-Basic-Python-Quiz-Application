ec0ffd3deb09617986ea709780290f38ff4d875f
Muzaffar Ahmad Tahir <muzaffarahmad709@gmail.com>
2023-10-13 10:58:17 +0000

    add some functionalities in code for answers

--- run.py
+++ run.py
@@ -1,11 +1,35 @@
 #  Game functions----
 
 def new_game():
-    pass
+
+    Suppos = []
+    correct_Suppos = 0
+    question_num = 1
+
+    for key in questions:
+        print(" ")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter (A, B, C, or D): ")
+        guess = guess.upper()
+        Suppos.append(guess)
+
+        correct_Suppos += check_answer(questions.get(key), guess)
+        question_num += 1
+
+    display_score(correct_Suppos, Suppos)
+
 
 #  Answer functions----    
-def check_answer():
-    pass
+def check_answer(answer, guess):
+
+    if answer == guess:
+        print("CORRECT!")
+        return 1
+    else:
+        print("WRONG!")
+        return 0
 
 #  display functions----    
 def display_score():
@@ -26,4 +50,4 @@ questions = {
 Answers = [["A. Ottawa", "B. Toronto", "C. Vancouver", "D. Calgary"],
           ["A. 8", "B. 7", "C. 20", "D. 16"],
           ["A. China", "B. Srilanka", "C. Between Nepal and Tibet", "D. Europe"],
-          ["A. True","B. False", "C. sometimes", "D. What's Earth?"]]
+          ["A. True","B. False", "C. sometimes", "D. What's Earth?"]]  
