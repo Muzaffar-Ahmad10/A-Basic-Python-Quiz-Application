be0f34d11566ef28587133b8628dcfba686c3f9f
Muzaffar Ahmad Tahir <muzaffarahmad709@gmail.com>
2023-10-13 09:20:01 +0000

    Add Questions and Answers

--- run.py
+++ run.py
@@ -1,8 +1,29 @@
+#  Game functions----
+
 def new_game():
     pass
+
+#  Answer functions----    
 def check_answer():
     pass
+
+#  display functions----    
 def display_score():
     pass
+
+#  play again functions----    
 def play_again():
-    pass        
+    pass  
+
+# Question and Answers ---
+questions = {
+ "Which city is the capital of Canada ?: ": "A",
+ "How many continents in the world ?: ": "B",
+ "Where is the highest mountain in the world located ?: ": "C",
+ "Is the Earth round?: ": "A"
+}
+
+Answers = [["A. Ottawa", "B. Toronto", "C. Vancouver", "D. Calgary"],
+          ["A. 8", "B. 7", "C. 20", "D. 16"],
+          ["A. China", "B. Srilanka", "C. Between Nepal and Tibet", "D. Europe"],
+          ["A. True","B. False", "C. sometimes", "D. What's Earth?"]]
